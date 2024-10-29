f1f3b1e30b350df50de0e8db555cea315fcb2173
tmgmgmg <tmgmgmg.01414@gmail.com>
2024-10-29 22:21:30 +0900

    microCMSの画像取得可能にする

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,17 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
   },
+    images: {
+    domains: ['images.microcms-assets.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
